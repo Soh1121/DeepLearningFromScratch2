1203618f014c2a5899bb4aef53391144e7707f6d
Soh1121 <satou.shg@gmail.com>
2020-12-09 11:50:58 +0900

    [add]ニューラルネットワークの学習に必要なクラスを実装

---
+++ common/layers.py
@@ -0,0 +1,19 @@
+from common.functions import softmax, cross_entropy_error
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None  # softmaxの出力
+        self.t = None  # 教師ラベル
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        # 教師ラベルがone-hotベクトルの場合、正解のインデックスに変換
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
