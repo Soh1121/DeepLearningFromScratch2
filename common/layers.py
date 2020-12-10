3e7a63110cdef05d64d1475ab24bca08fd6b98fb
Soh1121 <satou.shg@gmail.com>
2020-12-10 15:20:27 +0900

    [update]Sigmoidレイヤを実装

--- common/layers.py
+++ common/layers.py
@@ -21,6 +21,21 @@ class MatMul:
         return dx
 
 
+class Sigmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
 class SoftmaxWithLoss:
     def __init__(self):
         self.params, self.grads = [], []
