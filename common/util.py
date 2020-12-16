4f4ff1919c29497632eaaaa765e0862ca72965ee
Soh1121 <satou.shg@gmail.com>
2020-12-16 14:04:15 +0900

    [update]共起行列の生成

--- common/util.py
+++ common/util.py
@@ -17,3 +17,30 @@ def preprocess(text):
     corpus = np.array([word_to_id[w] for w in words])
 
     return corpus, word_to_id, id_to_word
+
+
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    '''共起行列の作成
+
+    :param corpus: コーパス（単語IDのリスト）
+    :param vocab_size:語彙数
+    :param window_size:ウィンドウサイズ（ウィンドウサイズが1のときは、単語の左右1単語がコンテキスト）
+    :return: 共起行列
+    '''
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
