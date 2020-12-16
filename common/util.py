cd40f7c3ab021236795862e2bc6f77fabecdceba
Soh1121 <satou.shg@gmail.com>
2020-12-16 14:09:01 +0900

    [add]コサイン類似度を実装

--- common/util.py
+++ common/util.py
@@ -19,6 +19,19 @@ def preprocess(text):
     return corpus, word_to_id, id_to_word
 
 
+def cos_similarity(x, y, eps=1e-8):
+    '''コサイン類似度の算出
+
+    :param x: ベクトル
+    :param y: ベクトル
+    :param eps: ”0割り”防止のための微小値
+    :return:
+    '''
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
+
+
 def create_co_matrix(corpus, vocab_size, window_size=1):
     '''共起行列の作成
 
