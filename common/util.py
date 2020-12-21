659f9cb5091e8ca8ee8f50d0a7e6190ca564f8cf
Soh1121 <satou.shg@gmail.com>
2020-12-21 14:04:38 +0900

    [update]コンテキストとターゲットを作成する関数を実装

--- common/util.py
+++ common/util.py
@@ -116,3 +116,24 @@ def ppmi(C, verbose=False, eps = 1e-8):
                 if cnt % (total//100 + 1) == 0:
                     print('%.1f%% done' % (100*cnt/total))
     return M
+
+
+def create_contexts_target(corpus, window_size=1):
+    '''コンテキストとターゲットの作成
+
+    :param corpus: コーパス（単語IDのリスト）
+    :param window_size: ウィンドウサイズ（ウィンドウサイズが1のときは、単語の左右1単語がコンテキスト）
+    :return:
+    '''
+    target = corpus[window_size: -window_size]
+    contexts = []
+
+    for idx in range(window_size, len(corpus) - window_size):
+        cs = []
+        for t in range(-window_size, window_size + 1):
+            if t == 0:
+                continue
+            cs.append(corpus[idx + t])
+        contexts.append(cs)
+
+    return np.array(contexts), np.array(target)
