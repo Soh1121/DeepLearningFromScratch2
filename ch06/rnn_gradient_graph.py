f43c43862fd3eb47aed1d19e2d36d70e7a28e59c
Soh1121 <satou.shg@gmail.com>
2021-01-05 11:37:32 +0900

    [update]勾配消失の確認

--- ch06/rnn_gradient_graph.py
+++ ch06/rnn_gradient_graph.py
@@ -8,7 +8,8 @@ T = 20  # 時系列データの長さ
 
 dh = np.ones((N, H))
 np.random.seed(3)
-Wh = np.random.randn(H, H)
+# Wh = np.random.randn(H, H)
+Wh = np.random.randn(H, H) * 0.5
 
 norm_list = []
 for t in range(T):
