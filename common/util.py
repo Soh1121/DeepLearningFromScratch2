49a0509ebf89b1db25ec056e7c209ed6d02482a5
Soh1121 <satou.shg@gmail.com>
2021-01-05 11:42:07 +0900

    [add]勾配爆発対策の実装

--- common/util.py
+++ common/util.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('..')
 from common.np import *
 
 
@@ -160,6 +162,18 @@ def create_contexts_target(corpus, window_size=1):
         contexts.append(cs)
 
     return np.array(contexts), np.array(target)
+
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
 
 
 def analogy(a, b, c, word_to_id, id_to_word, word_matrix, top=5, answer=None):
